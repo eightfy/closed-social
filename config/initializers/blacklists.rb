3142db432d155fc2397585703a4b8eeaba648bc2
Z <tdxdxoz@gmail.com>
2019-10-25 15:28:33 +0800

    set default email domain in ENV

--- config/initializers/blacklists.rb
+++ config/initializers/blacklists.rb
@@ -3,4 +3,5 @@
 Rails.application.configure do
   config.x.email_domains_blacklist = ENV.fetch('EMAIL_DOMAIN_BLACKLIST') { 'mvrht.com' }
   config.x.email_domains_whitelist = ENV.fetch('EMAIL_DOMAIN_WHITELIST') { '' }
+  config.x.email_default_domain = ENV.fetch('EMAIL_DEFAULT_DOMAIN') { '???.edu.cn' }
 end
--- public/auto_comp_email.js
+++ public/auto_comp_email.js
@@ -1,4 +1,6 @@
-var em = document.getElementById("user_email");
+var em = document.getElementById("registration_user_email");
+if(!em)
+  em = document.getElementById("user_email");
 var ap = em.nextSibling;
 em.addEventListener("blur", function( event ) {
 	if(ap.style.display != 'none' && em.value) {
