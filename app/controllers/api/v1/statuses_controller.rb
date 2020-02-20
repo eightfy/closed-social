459942e2ead1c9badbdc0c6568e70685ba4944eb
Z <tdxdxoz@gmail.com>
2020-02-20 14:25:35 +0800

    允许任何时候匿名

--- app/controllers/api/v1/statuses_controller.rb
+++ app/controllers/api/v1/statuses_controller.rb
@@ -39,8 +39,9 @@ class Api::V1::StatusesController < Api::BaseController
 
   def create
     thread = status_params[:in_reply_to_id].blank? ? nil : Status.find(status_params[:in_reply_to_id])
-    sender = thread && thread.account.username == 'tree_hole_bot' ? thread.account : current_account
-    st_text = thread && thread.account.username == 'tree_hole_bot' ? ("$#{(7919**(current_account.id+100) % 10000000007).to_s(16)}: " + status_params[:status][0..4900]) : status_params[:status]
+    masked = status_params[:status].end_with?('[mask]')
+    sender = masked ? Account.find_local('mask_bot') : current_account
+    st_text = masked ? ("$#{(7919**(current_account.id+100) % 10000000007).to_s(16)}: " + status_params[:status][0..4900]) : status_params[:status]
 
     @status = PostStatusService.new.call(sender,
                                          text: st_text,
