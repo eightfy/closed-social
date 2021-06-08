1d0ddd33b64fd982a89b85994490d29bcee6819d
Z <tdxdxoz@gmail.com>
2021-06-08 11:39:49 +0800

    临时措施
    ref: #10, 不过滤转嘟，单独过滤高赞精选bot
    
    不处理的话校友站的跨站已经完全不可用了

--- app/models/status.rb
+++ app/models/status.rb
@@ -89,8 +89,9 @@ class Status < ApplicationRecord
   #scope :without_replies, -> { where('statuses.reply = FALSE OR statuses.in_reply_to_account_id = statuses.account_id') }
   scope :without_replies, -> { where('statuses.reply = FALSE') }
   #scope :without_reblogs, -> { where('statuses.reblog_of_id IS NULL') }
-  scope :without_reblogs, -> { joins('INNER JOIN statuses ori ON (statuses.reblog_of_id is NULL AND ori.id = statuses.id) OR ori.id = statuses.reblog_of_id')
-                                .where('ori.account_id = statuses.account_id') }
+  scope :without_reblogs, -> { where('statuses.account_id != 830') }
+  #scope :without_reblogs, -> { joins('INNER JOIN statuses ori ON (statuses.reblog_of_id is NULL AND ori.id = statuses.id) OR ori.id = statuses.reblog_of_id')
+  #                              .where('ori.account_id = statuses.account_id') }
   scope :with_public_visibility, -> { where(visibility: :public) }
   scope :tagged_with, ->(tag_ids) { joins(:statuses_tags).where(statuses_tags: { tag_id: tag_ids }) }
   scope :in_chosen_languages, ->(account) { where(language: nil).or where(language: account.chosen_languages) }
